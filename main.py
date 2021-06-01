6f639c5ad6cf498547d9ff4380d7a915155a9dd5
gnehciak <54498709+gnehciak@users.noreply.github.com>
2021-06-01 23:02:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import tkinter as tk
 
 main_window = tk.Tk()
 
-main_window.grid_rowconfigure(2, weight=1)
-main_window.grid_columnconfigure(0, weight=1)
+main_window.geometry("600x400")
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -18,30 +17,32 @@ def enter():
 # Number
 # ----------------------------------------------------------------------------------------------------------------------
 
-def addnum(n, pword):
-    if len(pword.get()) < 4:
-        pword.set(pword.get() + str(n))
+def addnum(n, out):
+    if len(out.get()) < 4:
+        out.set(out.get() + str(n))
 
 
-def revnum(pword):
-    pword.set(pword.get()[0:-1])
+def revnum(out):
+    out.set(out.get()[0:-1])
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Main Frame /// Screen
 # ----------------------------------------------------------------------------------------------------------------------
 
+main_window.columnconfigure(0, weight=1)
+main_window.rowconfigure(1, weight=1)
+
+
 frame_1 = tk.Frame(
-    master=main_window,
+    main_window,
     background="black",
-    height="40",
-    width="80"
 )
 label_1 = tk.Label(
-    master=frame_1,
+    frame_1,
     text="Lorem ipsum \ndolor sit amet, \nconsectetur adipiscing\n elit."
 )
-label_1.pack()
+label_1.pack(fill=tk.BOTH)
 
 # Password field/Entry
 pword = tk.StringVar()
@@ -49,7 +50,7 @@ pentry = tk.Entry(
     frame_1,
     textvariable=pword
 )
-pentry.pack()
+pentry.pack(fill=tk.BOTH)
 
 frame_1.grid(row=0, column=1, sticky="news")
 
@@ -64,66 +65,69 @@ bottom_frame = tk.Frame(
 )
 bottom_frame.grid(row=1, column=1, sticky="news", padx=10, pady=10)
 
-main_window.columnconfigure(1, weight=1, minsize=75)
-main_window.rowconfigure(1, weight=1, minsize=50)
+main_window.columnconfigure(1, weight=1)
+main_window.rowconfigure(1, weight=1)
 
 # Generate buttons for 1-9
 num = 1
 for i in range(3):
-    bottom_frame.columnconfigure(i, weight=1, minsize=75)
-    bottom_frame.rowconfigure(i, weight=1, minsize=50)
+    bottom_frame.columnconfigure(i, weight=1)
+    bottom_frame.rowconfigure(i, weight=1)
     for j in range(3):
         button = tk.Button(
             master=bottom_frame,
             text=num,
             command=lambda x=num: addnum(x, pword)
         )
-        button.grid(row=i, column=j, sticky="news")
+        button.grid(row=i, column=j, sticky="news", padx=5, pady=5)
         num += 1
 
 # Generate button 0
-bottom_frame.columnconfigure(0, weight=1, minsize=75)
-bottom_frame.rowconfigure(3, weight=1, minsize=50)
+bottom_frame.columnconfigure(0, weight=1)
+bottom_frame.rowconfigure(3, weight=1)
 button = tk.Button(
     master=bottom_frame,
     text=0,
     command=lambda x=0, y=pword: addnum(x, y)
 )
-button.grid(row=3, column=0, columnspan=3, sticky="news")
+button.grid(row=3, column=0, columnspan=3, sticky="news", padx=5, pady=5)
 
 # Generate Enter button
-bottom_frame.columnconfigure(3, weight=1, minsize=75)
-bottom_frame.rowconfigure(2, weight=1, minsize=50)
+bottom_frame.columnconfigure(3, weight=1)
+bottom_frame.rowconfigure(2, weight=1)
 button = tk.Button(
     master=bottom_frame,
     text="Enter",
     command=enter()
 )
-button.grid(row=2, column=3, rowspan=2)
+button.grid(row=2, column=3, rowspan=2, sticky="news", padx=5, pady=5)
 
 # Generate Cancel button
-bottom_frame.columnconfigure(3, weight=1, minsize=75)
-bottom_frame.rowconfigure(2, weight=1, minsize=50)
+bottom_frame.columnconfigure(3, weight=1)
+bottom_frame.rowconfigure(2, weight=1)
 button = tk.Button(
     master=bottom_frame,
     text="Cancel",
     command=lambda: revnum(pword)
 )
-button.grid(row=1, column=3)
+button.grid(row=1, column=3, sticky="news", padx=5, pady=5)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Left Frame
 # ----------------------------------------------------------------------------------------------------------------------
 
 left_frame = tk.Frame(
-    background="blue"
+    main_window,
 )
-left_frame.grid(row=0, column=0)
+left_frame.grid(row=0, column=0, sticky="news", padx=10, pady=10)
+
 main_window.columnconfigure(0, weight=1)
 main_window.rowconfigure(0, weight=1)
 
+# Generate buttons
+
 for i in range(3):
-    left_frame.columnconfigure(i, weight=1)
+    left_frame.columnconfigure(0, weight=1)
     left_frame.rowconfigure(i, weight=1)
 
 lbtn_1 = tk.Button(
@@ -149,12 +153,35 @@ lbtn_3.grid(row=2, column=0, sticky="news")
 # ----------------------------------------------------------------------------------------------------------------------
 
 right_frame = tk.Frame(
-    background="blue"
+    main_window
 )
-right_frame.grid(row=0, column=2)
-main_window.columnconfigure(2, weight=1, minsize=75)
-main_window.rowconfigure(0, weight=1, minsize=50)
+right_frame.grid(row=0, column=2, sticky="news", padx=5, pady=5)
+main_window.columnconfigure(2, weight=1)
+main_window.rowconfigure(0, weight=1)
 
+# Generate buttons
+
+for i in range(3):
+    right_frame.columnconfigure(0, weight=1)
+    right_frame.rowconfigure(i, weight=1)
+
+rbtn_1 = tk.Button(
+    right_frame,
+    text="<<"
+)
+rbtn_1.grid(row=0, column=0, sticky="news")
+
+rbtn_2 = tk.Button(
+    right_frame,
+    text="<<"
+)
+rbtn_2.grid(row=1, column=0, sticky="news")
+
+rbtn_3 = tk.Button(
+    right_frame,
+    text="<<"
+)
+rbtn_3.grid(row=2, column=0, sticky="news")
 
 
 
